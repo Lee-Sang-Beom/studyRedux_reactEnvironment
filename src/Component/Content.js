690a097ec57718523a005533a06c9e76d3f98f80
Lee-Sang-Beom <tkdqja975@naver.com>
2022-10-29 13:55:32 +0900

    Modularization of the content.js jsx part

--- src/Component/Content.js
+++ src/Component/Content.js
@@ -1,37 +1,74 @@
 function Content() {
+  const onClickEvent = (e) => {
+    e.preventDefault();
+  };
+
+  const returnSubject = () => {
+    return (
+      <header>
+        <h1>Web</h1>
+        <p>Hello web!</p>
+      </header>
+    );
+  };
+
+  const returnTOC = () => {
+    return (
+      <nav>
+        <ol>
+          <li>
+            <a href="/" onClick={onClickEvent}>
+              HTML
+            </a>
+          </li>
+          <li>
+            <a href="/" onClick={onClickEvent}>
+              CSS
+            </a>
+          </li>
+        </ol>
+      </nav>
+    );
+  };
+
+  const returnControl = () => {
+    return (
+      <ul>
+        <li>
+          <a href="/" onClick={onClickEvent}>
+            create
+          </a>
+        </li>
+        <li>
+          <input type="button" value="delete" />
+        </li>
+      </ul>
+    );
+  };
+
+  const returnContent = () => {
+    return (
+      <article>
+        <h2>HTML</h2>
+        HTML is...
+      </article>
+    );
+  };
+
   return (
-    <>
+    <div className="container">
       {/* subject */}
-      <div id="subject">
-        <header>
-          <h1>Web</h1>
-          Hello web!
-        </header>
-      </div>
+      <div id="subject">{returnSubject()}</div>
 
       {/* toc */}
-      <div id="TOC">
-        <nav>
-          <ol></ol>
-        </nav>
-      </div>
+      <div id="TOC">{returnTOC()}</div>
 
       {/* control */}
-      <div id="control">
-        <ul>
-          <li>
-            <a href="#">create</a>
-          </li>
-          <li>
-            <input type="button" value="delete" />
-          </li>
-        </ul>
-      </div>
+      <div id="control">{returnControl()}</div>
 
       {/* content */}
-      <div id="content"></div>
-      <style jsx>{``}</style>
-    </>
+      <div id="content">{returnContent()}</div>
+    </div>
   );
 }
 
